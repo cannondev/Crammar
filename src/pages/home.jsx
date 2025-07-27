60ba89d7f3452d650974866a8b72701c9cddd529
Thomas Clark <thomas.c.clark.27@dartmouth.edu>
2025-07-27 15:26:55 -0400

    merge with homeScreen branch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <Flex flexDirection="column" width="100%" height="100%" justifyContent="space-evenly">
       <Box color="brand.10" width="100%" padding="4">
-        <Text textAlign="center" fontSize="9xl">Read. Fast.</Text>
+        <Text textAlign="center" fontSize="8xl">Read. Real Fast.</Text>
       </Box>
       <Box width="100%" padding="4" color="brand.100">
         <Text textAlign="center" fontSize="4xl">
