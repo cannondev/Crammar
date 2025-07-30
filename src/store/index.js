deceb53c1765fbeee27ecb609c6e8f0220293ed2
Thomas Clark <thomas.c.clark.27@dartmouth.edu>
2025-07-30 16:18:20 -0400

    routed back to render api

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,7 @@ import { immer } from 'zustand/middleware/immer';
 import { toast } from 'react-toastify';
 import axios from 'axios';
 
-// const ROOT_URL = 'https://api-crammar.onrender.com/api';
-const ROOT_URL = 'http://localhost:9090/api'; // for local development
+const ROOT_URL = 'https://api-crammar.onrender.com/api';
 
 const useStore = create(
   devtools(
